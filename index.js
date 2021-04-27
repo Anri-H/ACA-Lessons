4d52eddb3de5bb1a6e2da4e537dd9cbd8b897d92
Anri <anrihunanyan@gmail.com>
2021-04-27 17:58:44 +0400

    arr sub loop

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-console.log("hi");
-
-for (let i = 0; i < array; i++) {
-  const element = array[i];
+let arr = [12, 10, 2];
+let result = 0;
+for (let i = 0; i < arr.length; i++) {
+  result += arr[i];
 }
+console.log(result);
