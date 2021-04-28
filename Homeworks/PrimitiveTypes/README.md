fa5ec9ba318d2eeb0c074546fecf24ee0003e23a
Hunanyan Eduard <anrihunanyan@gmail.com>
2021-04-28 22:30:58 +0400

    feat: creted chapter tasks

--- Homeworks/PrimitiveTypes/README.md
+++ Homeworks/PrimitiveTypes/README.md
@@ -31,3 +31,4 @@ let a = 3;
 let b = 4;
 console.log(a > b); // false
 ```
+
--- Homeworks/PrimitiveTypes/primitive-types.js
+++ Homeworks/PrimitiveTypes/primitive-types.js
@@ -26,10 +26,4 @@ b = b - a;
 console.log("a = " + a + ", b = " + b);
 // a = 3, b =10
 
-5 > 4 //true
-"apple" > "pineapple" //false
-"2" > "12"  //true
-undefined == null //true
-undefined === null  //false
-null == "\n0\n" //false
-null === +"\n0\n" //false
+
