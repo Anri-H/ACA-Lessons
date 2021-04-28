9a560580230187af5081187eea41953bbb53ba75
Anri <anrihunanyan@gmail.com>
2021-04-28 15:57:04 +0400

    feat: added new math func

--- Homeworks/PrimitiveTypes/primitive-types.js
+++ Homeworks/PrimitiveTypes/primitive-types.js
@@ -6,3 +6,22 @@ console.log(admin);
 //What will be output of i and n?
 let i = 10;
 let n = i++ % 5; // i = 10 , n = 0
+/*
+//Swap two variables
+let a = 10,
+  b = 3;
+let c = a;
+a = b;
+b = c;
+console.log("a = " + a + ", b = " + b);
+// a = 3, b =10
+*/
+
+//Swap two variables without using the third one
+let a = 10,
+  b = 3;
+b = a + b;
+a = b - a;
+b = b - a;
+console.log("a = " + a + ", b = " + b);
+// a = 3, b =10
