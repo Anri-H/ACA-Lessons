e112e5512e1bf57cf1aefdcbce206719e41a16d5
Anri <anrihunanyan@gmail.com>
2021-04-29 16:47:40 +0400

    sum num + or -

--- Homeworks/ReferenceType,Branching,Loops/Loops.js
+++ Homeworks/ReferenceType,Branching,Loops/Loops.js
@@ -1,3 +1,15 @@
 let a = 40;
 let b = 60;
 console.log(180 - a - b);
+
+
+let num = [-4, 3, 7];
+let result = 1;
+for (let i = 0; i < num.length; i++) {
+  result *= num[i];
+}
+if (result < 0) {
+  console.log("-");
+} else if (result > 0) {
+  console.log("+");
+} else console.log("unsigned");
