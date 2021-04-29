daa8050754e7dae8b805cb8617eee7eab4913a40
Anri <anrihunanyan@gmail.com>
2021-04-29 15:52:09 +0400

    feat: multiple num 3,5,7

--- Homeworks/ReferenceType,Branching,Loops/Branching.js
+++ Homeworks/ReferenceType,Branching,Loops/Branching.js
@@ -7,16 +7,29 @@ if (isNaN(number)) {
   alert("odd");
 } else alert("even");
 
-
 // Given a positive integer. Bring the last digit of the numberto the beginning. Print the new number. If the
 // last digit of the inserted numberis 0, numberremains the same.
 
-let num = 3074;
-let arr = num.toString().split("");
+let numChange = 3074;
+let arr = numChange.toString().split("");
 let lastNum = arr.pop();
 if (lastNum !== "0") {
   arr.unshift(lastNum);
   console.log(arr.join(""));
-} else console.log(num);
+} else console.log(numChange);
+
+//Check if a numberis a multiple of 3, 5 or 7 and output the appropriate message.
+
+const numberA = 21;
+
+if (numberA % 3 === 0 && numberA % 5 === 0 && numberA % 7 === 0) {
+  console.log(numberA + " is a multiple of 3, 5 and 7");
+} else if (numberA % 3 === 0) {
+  console.log(numberA + " is a multiple of 3");
+} else if (numberA % 5 === 0 && numberA % 7 === 0) {
+  console.log(numberA + " is a multiple of 5 and 7");
+} else if (numberA % 3 === 0 && numberA % 7 === 0) {
+  console.log(numberA + " is a multiple of 3 and 7");
+} else console.log(numberA + " is not a multiple of 3, 5 or 7");
 
 
