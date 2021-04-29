2757470f127115bcc75cccc5cc0b43049c055a6c
Anri <anrihunanyan@gmail.com>
2021-04-29 18:44:34 +0400

    fix: change reverse num

--- Homeworks/ReferenceType,Branching,Loops/Loops.js
+++ Homeworks/ReferenceType,Branching,Loops/Loops.js
@@ -26,13 +26,3 @@ function digitNumber(digit, number) {
   return console.log("No");
 }
 digitNumber(7, 245);
-
-//Enter a number. Reverse its first and last digits. Print the new number.
-
-let reverseNu = 1423464635;
- let reverseNum = reverseNu.toString().split("");
-let first = reverseNum.shift();
-let end = reverseNum.pop();
-reverseNum.push(first);
-reverseNum.unshift(end);
-console.log(reverseNum.join());
--- Homeworks/ReferenceType,Branching,Loops/referenceType.js
+++ Homeworks/ReferenceType,Branching,Loops/referenceType.js
@@ -1,1 +1,11 @@
 
+
+//Enter a number. Reverse its first and last digits. Print the new number.
+
+let reverseNu = 1423464635;
+ let reverseNum = reverseNu.toString().split("");
+let first = reverseNum.shift();
+let end = reverseNum.pop();
+reverseNum.push(first);
+reverseNum.unshift(end);
+console.log(reverseNum.join());
