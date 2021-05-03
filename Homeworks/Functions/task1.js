4bea11149f97edde3e6412efdc50306d732048aa
Hunanyan Eduard <anrihunanyan@gmail.com>
2021-05-03 21:06:42 +0400

    feat:created task1.js file & created separed odd & even function

---
+++ Homeworks/Functions/task1.js
@@ -0,0 +1,13 @@
+//Given an array of numbers. Write a function to separate odd and even numbers in different arrays.
+
+function separate(arr) {
+  let odd = [];
+  let even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      odd.push(arr[i]);
+    } else even.push(arr[i]);
+  }
+  return odd;
+}
+console.log(separate([45, 12, 3, 6, 17, 0]));
