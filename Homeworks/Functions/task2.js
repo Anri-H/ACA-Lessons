52cd7fc92c19a1dd7763ff922773742313941fd9
Hunanyan Eduard <anrihunanyan@gmail.com>
2021-05-03 21:12:28 +0400

    feat: created difference func

--- Homeworks/Functions/task2.js
+++ Homeworks/Functions/task2.js
@@ -2,9 +2,17 @@
 // depending on passed operation symbol. Write appropriate function for each operation
 
 function sum(arr) {
-  let result = 0;
-  for (let i = 0; i < arr.length; i++) {
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i++) {
     result += arr[i];
+  }
+  return result;
+}
+
+function difference(arr) {
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    result -= arr[i];
   }
   return result;
 }
