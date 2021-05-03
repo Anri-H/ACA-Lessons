becd3724ef68385b55eda39f4922fe578948d9f7
Anri <anrihunanyan@gmail.com>
2021-05-03 17:17:18 +0400

    fix:correct parse int func

--- Homeworks/Type Coercion for Primitives/parseint.js
+++ Homeworks/Type Coercion for Primitives/parseint.js
@@ -1,15 +1,24 @@
-let str = "   0xffhhh";
+let str = "  0XFdf";
 let res = "";
 for (let i = 0; i < str.length; i++) {
-  if (isNaN(str[0])) {
-    res = NaN;
-    break;
-  } else if ((str[i] === "x" && str[i - 1] === "0") || str[i] <= "f") {
+  if (str[i] === " ") {
+    continue;
+  } else if (
+    (str[i] ? "x" : "X" && str[i - 1] === "0") ||
+    (str[i] <= "f" && res[1] ? "x" : "X")
+  ) {
+    if (isNaN(str[i]) && res[0] !== "0") {
+      break;
+    }
     res += str[i];
   } else if (isNaN(str[i])) {
+    if (res[0] === undefined) {
+      res = NaN;
+      break;
+    }
     break;
   } else res += str[i];
 }
 
 console.log(Number(res));
-console.log(parseInt("0xffhhh"));
+console.log(parseInt("   0Xffhj"));
