dc9918619838217eec8d8565b7463ec513423729
Anri <anrihunanyan@gmail.com>
2021-05-04 17:16:04 +0400

    feat: largeNum function task9

--- Homeworks/Functions/task9.js
+++ Homeworks/Functions/task9.js
@@ -1,2 +1,14 @@
 // Write a function which receives an array and a number as arguments and returns a new array from the
 // elements of the given array which are largerthan the given number.
+
+function largeNum(arr, num) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > num) {
+            newArr.push(arr[i])
+        }
+        
+    }
+    return newArr
+}
+console.log(largeNum([10, 25, 16, -5, 30, 15, 24] , 16))
