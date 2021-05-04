6bbf5b6d3c53234e4ef9140f4a11c8cb05209605
Anri <anrihunanyan@gmail.com>
2021-05-04 13:30:12 +0400

    fix: corrected task2 calculate func

--- Homeworks/Functions/task2.js
+++ Homeworks/Functions/task2.js
@@ -1,6 +1,21 @@
 // Write a function that calculates sum, difference, multiplication and division between given array elements
 // depending on passed operation symbol. Write appropriate function for each operation
 
+function calculate(aperation, arr) {
+  switch (aperation) {
+    case "+":
+      return sum(arr);
+    case "-":
+      return difference(arr);
+    case "*":
+      return multiplication(arr);
+    case "/":
+      return division(arr);
+    default:
+      break;
+  }
+}
+
 function sum(arr) {
   let result = arr[0];
   for (let i = 1; i < arr.length; i++) {
@@ -18,17 +33,19 @@ function difference(arr) {
 }
 
 function multiplication(arr) {
-    let result = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-      result *= arr[i];
-    }
-    return result;
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    result *= arr[i];
   }
+  return result;
+}
 
-  function division(arr) {
-    let result = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-      result /= arr[i];
-    }
-    return result;
+function division(arr) {
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    result /= arr[i];
   }
+  return result;
+}
+
+console.log(calculate("*", [45, 12, 3, 6, 17]));
