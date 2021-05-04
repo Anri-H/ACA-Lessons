32563bef6e6dc670c7ac460c22f9009fccf20032
Anri <anrihunanyan@gmail.com>
2021-05-04 15:18:36 +0400

    feat:created task8.js file

---
+++ Homeworks/Functions/task8.js
@@ -0,0 +1,3 @@
+// Write a function that accepts a string(a sentence) as a parameter and finds the longest word within the
+// string. If there are several words which are the longest ones, print the last word(words can be separated
+// by space, comma or hyphen).
