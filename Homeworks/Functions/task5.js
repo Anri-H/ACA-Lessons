a2c5259d47aebfdea0cf772cff5ca0fe2d98b2be
Anri <anrihunanyan@gmail.com>
2021-05-04 15:12:15 +0400

    feat:unfinished findNumSum func task5

---
+++ Homeworks/Functions/task5.js
@@ -0,0 +1,25 @@
+// Write a function, which receives a string, finds possible largest numbers in the string and returns their
+// sum.
+
+function findNumSum(str) {
+  let result = 0;
+  let numbers = 0;
+  let num = "";
+  for (let i = 0; i < str.length; i++) {
+    if (isNaN(str[i]) && !isNaN(str[i - 1])) {
+      numbers += Number(num);
+      num = " ";
+    }
+
+    if (!isNaN(str[i]) && !isNaN(str[i - 1])) {
+      num += str[i];
+    }
+    if (!isNaN(str[i])) {
+      num = " ";
+      numbers += Number(str[i]);
+      num += str[i];
+    }
+  }
+  return numbers;
+}
+console.log(findNumSum("drtdg3hdsd32 jj 6hbghj23hv"));
