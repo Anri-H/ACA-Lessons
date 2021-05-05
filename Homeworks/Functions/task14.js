c65fc17c2ffec910575d03151816dfbb21b5654a
Anri <anrihunanyan@gmail.com>
2021-05-05 15:58:53 +0400

    feat: created task14.js file

---
+++ Homeworks/Functions/task14.js
@@ -0,0 +1,3 @@
+// Write a function, which receives an array as an argument which elements arrays of numbers. Find
+// product of biggest negative number of each array. If there is not any negative number, ignore that array.
+// Check that items of the given array are arrays.
