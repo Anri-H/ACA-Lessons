42ec7763bf7dd29154ca163e01824104fa929fd4
Anri <anrihunanyan@gmail.com>
2021-05-06 13:51:43 +0400

    feat: created redundant func in task 2

--- Homeworks/Closure/task2.js
+++ Homeworks/Closure/task2.js
@@ -1,1 +1,11 @@
 //Write a function redundant that takes in a string str and returns a function thatreturns str.
+
+function redundant(str) {
+  const foo = () => {
+    return str;
+  };
+  return foo(str);
+}
+
+const f1 = redundant("apple");
+console.log(f1);
