0b26cf5388b961018453bc77e2de37c90d1643fd
Anri <anrihunanyan@gmail.com>
2021-05-06 14:04:03 +0400

    feat:created add_suffix func in task 3

--- Homeworks/Closure/task3.js
+++ Homeworks/Closure/task3.js
@@ -1,2 +1,15 @@
 // Write a function thatreturns an anonymous function, which transforms its input by adding a particular
 // suffix at the end.
+
+function add_suffix(str) {
+  let part = str;
+  return (str) => {
+    return str + part;
+  };
+}
+
+const add_ly = add_suffix("ly");
+console.log(add_ly("hopeless"));
+
+add_less = add_suffix("less");
+console.log(add_less("fear"));
