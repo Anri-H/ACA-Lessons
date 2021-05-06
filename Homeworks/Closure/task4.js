4f56d197e3f58c166bb0791b8686b887a02ce910
Anri <anrihunanyan@gmail.com>
2021-05-06 14:14:16 +0400

    feat: created printAfter func in task 4

--- Homeworks/Closure/task4.js
+++ Homeworks/Closure/task4.js
@@ -1,1 +1,9 @@
 //Create a function printAfter that calls its argument after printing hello world
+
+function printAfter(print) {
+  console.log("hello world");
+  return print();
+}
+
+const print = () => console.log("Elon Musk");
+printAfter(print);
