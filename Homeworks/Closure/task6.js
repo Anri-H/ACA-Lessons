f2d8740e464aa62a9ac75a80bcab89c251055161
Anri <anrihunanyan@gmail.com>
2021-05-06 14:51:01 +0400

    feat: created filterByLength func in task 6

---
+++ Homeworks/Closure/task6.js
@@ -0,0 +1,13 @@
+//Write a function which remove elements with length <= 3
+
+function filterByLength(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length <= 3) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(filterByLength(["kia", "tesla", "bmw", "mercedes"]));
