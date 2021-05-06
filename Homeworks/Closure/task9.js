4bdb68abeb144e4ec7f2394ea12c48bb0516b002
Anri <anrihunanyan@gmail.com>
2021-05-06 16:05:12 +0400

    feat:created sumQuadraticEven func in task9

--- Homeworks/Closure/task9.js
+++ Homeworks/Closure/task9.js
@@ -1,1 +1,13 @@
 //Given an array of numbers. Find the sum of numbersʼ quadratic which are even.
+
+function sumQuadraticEven(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      result += arr[i];
+    }
+  }
+  return result;
+}
+
+console.log(sumQuadraticEven([12, 43, 22, 1]));
