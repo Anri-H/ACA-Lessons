033da68f859a74533e651f9eeda0fa3c5e26a6e4
Anri <anrihunanyan@gmail.com>
2021-05-06 15:45:27 +0400

    feat:created isogram func in task 8

--- Homeworks/Closure/task8.js
+++ Homeworks/Closure/task8.js
@@ -1,4 +1,18 @@
 // Determine if a word or phrase is an isogram. An isogram (also known as a "non pattern word") is a word
 // or phrase without a repeating letter.
 
+function isogram(str) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (i === j) {
+        continue;
+      }
+      if (str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
+console.log(isogram("qwertyuq"));
