1e232d17da89734621edb39755229fb10bec5185
Anri <anrihunanyan@gmail.com>
2021-05-11 14:57:54 +0400

    feat: created task5.js

---
+++ Homeworks/ArrayMethods/task5.js
@@ -0,0 +1,4 @@
+// - Create a function that takes an array of numbers arr, a string str
+// and return an array of numbers as per the following rules:
+// - "Asc" returns a sorted array in ascending order.
+// - "Desc" returns a sorted array in descending order.
