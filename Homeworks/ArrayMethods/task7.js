865e265ee7c35adcce8bc50ff5804a33cf35343d
Anri <anrihunanyan@gmail.com>
2021-05-11 16:37:54 +0400

    feat:numbersInclude func created

--- Homeworks/ArrayMethods/task7.js
+++ Homeworks/ArrayMethods/task7.js
@@ -1,2 +1,12 @@
 // Given an array, return a new array that only includes the numbers.
 
+const arr = [1, 2, "a", true, {}, undefined, 55, false, "hi", null, 166, NaN];
+
+function numbersInclude(arr) {
+  return arr.filter((el) => {
+      if (typeof el === "number") {
+          return el
+      }
+    });
+}
+console.log(numbersInclude(arr));
