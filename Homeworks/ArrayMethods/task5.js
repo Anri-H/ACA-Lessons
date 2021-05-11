7d3abee02545e0a8b91ca91b39a6d06b027b178a
Anri <anrihunanyan@gmail.com>
2021-05-11 15:14:11 +0400

    feat:sortBy func created

--- Homeworks/ArrayMethods/task5.js
+++ Homeworks/ArrayMethods/task5.js
@@ -2,3 +2,16 @@
 // and return an array of numbers as per the following rules:
 // - "Asc" returns a sorted array in ascending order.
 // - "Desc" returns a sorted array in descending order.
+
+function sortBy(arr, str) {
+  if (str === "Desc") {
+    const desc = (a, b) => a > b;
+    return arr.sort(desc);
+  }
+  if (str === "Asc") {
+    const asc = (a, b) => a - b;
+    return arr.sort(asc);
+  }
+}
+
+console.log(sortBy([4, 14, 2, 1], "Asc"));
