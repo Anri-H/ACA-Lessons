96c53863bb8244c83532e825a90ba3d59bc4ab83
Anri <anrihunanyan@gmail.com>
2021-05-12 17:02:34 +0400

    fix:corrected splice func

--- Homeworks/ArrayMethods/splice.js
+++ Homeworks/ArrayMethods/splice.js
@@ -1,16 +1,22 @@
-function splice(arr, start, deleteCount, ...item) {
+function splice(arr, start = 0, deleteCount = arr.length, ...item) {
   let newArr = [];
+  let spliceArr = [];
+  if (start < 0) {
+    start += arr.length;
+  }
+  let end = start + deleteCount;
   for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
-    if (!deleteCount && index === start) {
-      newArr.push(...item);
-    } else if (index === start) {
+    if (index >= start && index < end) {
+      spliceArr.push(element);
+    } else if (index === end) {
       newArr.push(...item);
       newArr.push(element);
-    } 
-    else newArr.push(element);
+    } else newArr.push(element);
   }
+  
   return newArr;
 }
 
-console.log(splice([1, 4, 3, 2, 5, 67], 3, 1, "ad", "dasda"));
+let arr = [1, 4, 3, 2, 5, 67];
+console.log(splice(arr, -3, 2, "ada", 24));
