3dd140c4198b0584728f0352700d5aca4d6f3969
Hunanyan Eduard <anrihunanyan@gmail.com>
2021-05-14 23:33:55 +0400

    feat:created groupBy func

--- Homeworks/Object/JSON/task4.js
+++ Homeworks/Object/JSON/task4.js
@@ -1,1 +1,15 @@
 //Write function that will group by some rule
+
+function groupBy(arr, func) {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    const key = func(element);
+    let values = arr.filter((el) => key === func(el));
+    obj[key] = values;
+  }
+  return obj;
+}
+
+console.log(groupBy([6.1, 4.2, 6.3], Math.floor));
+console.log(groupBy(["one", "two", "three"], (i) => i.length));
