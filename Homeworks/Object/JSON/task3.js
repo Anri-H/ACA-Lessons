52dffc559ad935cad4ff44353c96d33462d415ec
Hunanyan Eduard <anrihunanyan@gmail.com>
2021-05-14 22:28:52 +0400

    feat:created zipObject func & task3.js file

--- Homeworks/Object/JSON/task3.js
+++ Homeworks/Object/JSON/task3.js
@@ -1,4 +1,14 @@
 //Write function that will zip arrays into object
 
+function zipObject(key, val) {
+  let obj = key.reduce((acc, el, index) => {
+    if (val[index] === undefined) {
+      val[index] = null;
+    }
+    acc[el] = val[index];
+    return acc;
+  }, {});
+  return obj;
+}
 
-console.log(zipObject(["a", "b", "c"], [1, 2]))
+console.log(zipObject(["a", "b", "c"], [1, 2]));
