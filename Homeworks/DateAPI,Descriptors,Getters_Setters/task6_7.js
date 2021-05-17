8211f55dfe7c7accc05ad1c59b3dc891a975011f
Anri <anrihunanyan@gmail.com>
2021-05-17 18:18:22 +0400

    feat:created task6_7.js

---
+++ Homeworks/DateAPI,Descriptors,Getters_Setters/task6_7.js
@@ -0,0 +1,11 @@
+// Write a function that, given a date (in the format MM/DD/YYYY),returns the day of the week as a string.
+// Each day name must be one of the following strings: "Sunday", "Monday", "Tuesday", "Wednesday",
+// "Thursday", "Friday", or "Saturday".
+
+function getDay(str){
+    return new Date(str)
+
+}
+
+
+console.log(getDay("12/07/2016"))
