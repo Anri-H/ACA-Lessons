eb2e2826594e2d885faecb9bb958dcaa92fe58dd
Hunanyan Eduard <anrihunanyan@gmail.com>
2021-05-17 22:46:56 +0400

    feat:created task8_9.js & implemeted folowing functions

---
+++ Homeworks/DateAPI,Descriptors,Getters_Setters/task8_9.js
@@ -0,0 +1,12 @@
+// Implement following function
+
+const getWeekOfMonth = (date) => {return date};
+const result = getWeekOfMonth(new Date(2017, 10, 9));
+console.log(result);
+
+
+const formatDate = (date) => {
+    return date;
+    };
+console.log("Actual output: ", formatDate(new Date("2020-05-14")));
+console.log("Expected output", "14 May 2020");
