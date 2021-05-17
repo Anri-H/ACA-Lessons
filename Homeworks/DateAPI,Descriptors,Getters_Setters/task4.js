d619e437236c8105451437ea4c8939bdf8e67f79
Anri <anrihunanyan@gmail.com>
2021-05-17 17:49:42 +0400

    feat:created task4.js & monthsInterval

---
+++ Homeworks/DateAPI,Descriptors,Getters_Setters/task4.js
@@ -0,0 +1,29 @@
+//The function should produce the same output even if dateStart is greaterthan dateEnd
+
+function monthsInterval(start, end) {
+  let months = [
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+  ];
+  let res = [];
+  const monthStart = start.getMonth();
+  const monthEnd = end.getMonth();
+  for (let index = monthStart; index < monthEnd; index++) {
+    res.push(months[index]);
+  }
+  return res;
+}
+
+let january = new Date(2017, 0, 1);
+let march = new Date(2017, 2, 1);
+console.log(monthsInterval(january, march));
