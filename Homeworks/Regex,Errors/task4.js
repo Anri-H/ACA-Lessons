d1e9bb77d030a79785001e0eddf252f216fc8690
Davit <dav040201@gmail.com>
2021-05-18 22:22:20 +0400

    fix:corrected gradeLabs func

--- Homeworks/Regex,Errors/task4.js
+++ Homeworks/Regex,Errors/task4.js
@@ -1,32 +1,42 @@
-function gradeLabs(labs) {
-  try {
-    labs.forEach((element) => {
-      if (element[runLab] !== undefinded) {
-      }
-    });
-  } catch (error) {
-    console.log("Error thrown");
-  }
-  for (let i = 0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
-  }
-}
-
 let studentLabs = [
   {
-    student: "Carly",
+    student: "Blake",
+    myCode: function (num) {
+      return Math.pow(num, num);
+    },
+  },
+  {
+    student: "Jessica",
     runLab: function (num) {
       return Math.pow(num, num);
     },
   },
   {
-    student: "Erica",
+    student: "Mya",
     runLab: function (num) {
       return num * num;
     },
   },
 ];
+
+function gradeLabs(labs) {
+  try {
+    for (let i = 0; i < labs.length; i++) {
+      let lab = labs[i];
+      if (!lab.runLab) {
+        throw new Error("Error thrown");
+      } else if (typeof lab.runLab !== "function") {
+        throw new TypeError("lab.runLab is not a function.");
+      } else {
+        let result = lab.runLab(3);
+        console.log(`${lab.student} code worked: ${result === 27}`);
+      }
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 gradeLabs(studentLabs);
