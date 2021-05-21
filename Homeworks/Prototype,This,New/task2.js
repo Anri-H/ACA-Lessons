8ebf3a76ebe4b1d3ba3573347a70a75edab5a1fb
Anri-H <anrihunanyan@gmail.com>
2021-05-21 20:06:34 +0400

    fix:added compareAge func in person constructor

--- Homeworks/Prototype,This,New/task2.js
+++ Homeworks/Prototype,This,New/task2.js
@@ -13,6 +13,17 @@ function Person(name, age) {
   this.toString = () => {
     return `${this.name},${this.age}`;
   };
+  this.compareAge = (person) => {
+    if (this.age === person.age) {
+      return `${person.name} is the same age as me.`;
+    }
+    if (this.age > person.age) {
+      return `${person.name} is younger than me.`;
+    }
+    if (this.age < person.age) {
+      return `${person.name} is older than me.`;
+    }
+  };
 }
 
 const p1 = new Person("Samuel", 24);
@@ -21,3 +32,4 @@ const p3 = new Person("Lily", 24);
 p1.eat("duck");
 p1.poop();
 console.log(p1.toString());
+console.log(p1.compareAge(p2));
