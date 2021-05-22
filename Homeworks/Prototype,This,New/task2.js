d99d9afc591c93c5e86ee848b48585d2e88b2d82
Anri-H <anrihunanyan@gmail.com>
2021-05-22 22:11:44 +0400

    fix:corrected task1 & task2

--- Homeworks/Prototype,This,New/task2.js
+++ Homeworks/Prototype,This,New/task2.js
@@ -2,34 +2,37 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
   this.stomach = [];
-  this.eat = (food) => {
-    if (this.stomach.length < 10) {
-      this.stomach.push(food);
-    }
-  };
-  this.poop = () => {
-    this.stomach = [];
-  };
-  this.toString = () => {
-    return `${this.name},${this.age}`;
-  };
-  this.compareAge = (person) => {
-    if (this.age === person.age) {
-      return `${person.name} is the same age as me.`;
-    }
-    if (this.age > person.age) {
-      return `${person.name} is younger than me.`;
-    }
-    if (this.age < person.age) {
-      return `${person.name} is older than me.`;
-    }
-  };
 }
+
+Person.prototype.eat = function (food) {
+  if (this.stomach.length < 10) {
+    this.stomach.push(food);
+  }
+};
+
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function () {
+  return `${this.name},${this.age}`;
+};
+
+Person.prototype.compareAge = function (person) {
+  if (this.age === person.age) {
+    return `${person.name} is the same age as me.`;
+  }
+  if (this.age > person.age) {
+    return `${person.name} is younger than me.`;
+  }
+  if (this.age < person.age) {
+    return `${person.name} is older than me.`;
+  }
+};
 
 const p1 = new Person("Samuel", 24);
 const p2 = new Person("Joel", 36);
 const p3 = new Person("Lily", 24);
 p1.eat("duck");
-p1.poop();
-console.log(p1.toString());
+console.log(p1);
 console.log(p1.compareAge(p2));
--- Homeworks/Prototype,This,New/task3.js
+++ Homeworks/Prototype,This,New/task3.js
@@ -2,23 +2,24 @@ function Car(model, milesPerGallon = 0) {
   this.model = model;
   this.tank = 0;
   this.odometer = milesPerGallon;
-  this.fill = (gallons) => {
-    this.tank += gallons;
-  };
-  this.drive = (distanc) => {
-    if (this.tank - distanc < 0) {
-      let noFuel = this.tank;
-      this.odometer += noFuel;
-      this.tank = 0;
-      return `I ran out of fuel at ${noFuel} miles!`;
-    }
-    this.odometer += distanc;
-    this.tank -= distanc;
-  };
 }
+
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+};
+
+Car.prototype.drive = function(distanc) {
+  if (this.tank - distanc < 0) {
+    let noFuel = this.tank;
+    this.odometer += noFuel;
+    this.tank = 0;
+    return `I ran out of fuel at ${noFuel} miles!`;
+  }
+  this.odometer += distanc;
+  this.tank -= distanc;
+};
 
 const bmw = new Car("Bmw", 25);
 
 bmw.fill(40);
-console.log(bmw.drive(60));
 console.log(bmw);
