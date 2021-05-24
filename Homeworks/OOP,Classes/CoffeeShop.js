57c58d2e3b254b5997a987252627a81c4419b9d6
Anri <anrihunanyan@gmail.com>
2021-05-24 15:50:03 +0400

    fix:finished task 1 author & book class

---
+++ Homeworks/OOP,Classes/CoffeeShop.js
@@ -0,0 +1,5 @@
+function CoffeeShop(name, {menu}, orders) {
+  this.name = name;
+  this.menu = menu;
+  this.orders = orders;
+}
--- Homeworks/OOP,Classes/task1.js
+++ Homeworks/OOP,Classes/task1.js
@@ -1,35 +1,60 @@
-function Author(name, email, gender) {
-  this._name;
-  this.email = email;
-  this.gender = gender;
-  set _name(name) {
-    throw 
-  },
-  get _name() {
+class Author {
+  constructor(name, email, gender) {
+    this._name = name;
+    this.email = email;
+    this.gender = gender;
+  }
+  set name(val) {
+    throw Error("You can't changed Author name");
+  }
+  get name() {
     return this._name;
-  },
+  }
+  toString() {
+    return `Ms: ${this.name}`;
+  }
 }
 
-Author.prototype.toString = function () {
-  return `Ms: ${this.name}`;
-};
-
-
-function Book(title, author, price, quantity) {
-  this.title = title;
-  this.author = author;
-  this.price = price;
-  this.quantity = quantity;
+class Book {
+  constructor(title, author, price, quantity) {
+    this._title = title;
+    this._author = author;
+    this._price = price;
+    this._quantity = quantity;
+  }
+  getProfit() {
+    return this._quantity * this._price;
+  }
+  toString() {
+    return `${this._title}`;
+  }
+  set title(val) {
+    throw Error("You can't changed Book title");
+  }
+  get title() {
+    return this._title;
+  }
+  set author(val) {
+    throw Error("You can't changed Book author");
+  }
+  get author() {
+    return this._author;
+  }
+  set price(val) {
+    throw Error("You can't changed Book price");
+  }
+  get price() {
+    return this._price;
+  }
+  set quantity(val) {
+    throw Error("You can't changed Book quantity");
+  }
+  get quantity() {
+    return this._quantity;
+  }
 }
-
-Book.prototype.getters = ;
-
-Book.prototype.setters = ;
-
-Book.prototype.getProfit = function () {
-  return this.quantity * this.price;
-};
-
-Book.prototype.toString = function () {
-    return `${this.title}`
-}
+let author1 = new Author("J. K. Rowling", "abc@gmail.com", "fEmaLe");
+console.log(author1.gender); // female
+console.log(author1.name); // J. K. Rowling
+console.log(author1.toString()); // Ms. J. K. Rowling
+author.name = 12; 
