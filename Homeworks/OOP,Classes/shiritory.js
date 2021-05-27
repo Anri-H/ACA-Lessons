0e05e931ec7ec0554380de4c5c4560cee5527f7f
Anri-H <anrihunanyan@gmail.com>
2021-05-27 22:01:20 +0400

    feat:created shiritory classs

---
+++ Homeworks/OOP,Classes/shiritory.js
@@ -0,0 +1,42 @@
+class Shiritory {
+  constructor() {
+    this.words = [];
+    this.game_over = false;
+  }
+
+  play(val) {
+    if (!this.words.length) {
+      this.words.push(val);
+    }
+    const lastCharacter = this.words[this.words.length - 1];
+    const lastStr = lastCharacter.length - 1;
+    if (val[0] === lastCharacter[lastStr]) {
+      this.words.push(val);
+      return this.words;
+    } else {
+      this.game_over = true;
+      return "game over";
+    }
+  }
+
+  restart() {
+    this.words = [];
+    this.game_over = false;
+    return "game restarted";
+  }
+}
+
+let myShiritori = new Shiritory();
+console.log(myShiritori.play("apple")); // ["apple"]
+console.log(myShiritori.play("ear")); // ["apple", "ear"]
+console.log(myShiritori.play("rhino")); // ["apple", "ear", "rhino"]
+console.log(myShiritori.play("corn")); // "game over"
+// Corn does not start with an "o".
+console.log(myShiritori.words); // ["apple", "ear", "rhino"]
+// Words should be accessible.
+console.log(myShiritori.restart()); // "game restarted"
+console.log(myShiritori.words); // []
+// Words array should be set back to empty.
+console.log(myShiritori.play("hostess")); // ["hostess"]
+console.log(myShiritori.play("stash")); // ["hostess", "stash"]
+console.log(myShiritori.play("hostess")); // "game over"
--- Homeworks/index.js
+++ Homeworks/index.js
@@ -4,10 +4,14 @@ let user = {
   email: ["cdsfdsf", "fcsfcsc"],
 };
 
-let keys = Object.assign({},user);
+let keys = Object.assign({}, user);
 
 user.age = 12;
 user.email[2] = 15;
 
 console.log(user);
 console.log(keys);
+
+let arr = ["apple", "eat"];
+let last = arr[arr.length - 1];
+console.log(last.length-1);
